4b680054dc4a7f97411231c7bc350932d189be07
VietNT2011 <ntv142018@gmail.com>
2025-11-29 22:53:52 +0700

    update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,8 +1,19 @@
 import os
 import uvicorn
+import sys
 
 def start():
     uvicorn.run("main:app")
 
 def inngest_ui():
     os.system("npx inngest-cli@latest dev -u http://127.0.0.1:8000")
+
+# Only run when executed directly on command line
+if __name__ == "__main__":
+    command = sys.argv[1]
+    if command == "start":
+        start()
+    elif command == "inngest_ui":
+        inngest_ui()
+    else:
+        print(f"Not found: {command}")
